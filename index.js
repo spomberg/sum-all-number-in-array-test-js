890eec370c9371ca93472e3d32881ecd28433c51
Marcos Spomberg <marcosspomberg@gmail.com>
2021-11-05 16:41:08 +0000

    Working code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  const flattenedArr = array.flat(Infinity);
+  if(flattenedArr.length === 1) return flattenedArr[0];
+  return flattenedArr[0] + sumItems(flattenedArr.slice(1));
 }
 
 module.exports = sumItems;
